33299bbab4c65976715e25d47509ba7bd02bc015
Michael Fadely <michaelwfadely@gmail.com>
2018-10-08 08:36:02 -0600

    give me the interfaces

--- wlan-test/main.cpp
+++ wlan-test/main.cpp
@@ -3,14 +3,15 @@
 
 #include <wlanapi.h>
 
-#include <iostream>
+#include <atomic>
 #include <chrono>
-#include <thread>
+#include <iostream>
 #include <string>
+#include <thread>
 
 using namespace std::chrono;
 
-static bool scan_complete = false;
+static std::atomic<bool> scan_complete = false;
 
 // this callback will be used to wait for WlanScan to complete.
 static void wlan_callback(PWLAN_NOTIFICATION_DATA Arg1, PVOID Arg2)
@@ -68,95 +69,94 @@ int main(int argc, char** argv)
 
 	for (size_t i = 0; i < interface_list->dwNumberOfItems; ++i)
 	{
+		std::cout << "Interface " << i + 1 << ": ";
 		const auto& info = interface_list->InterfaceInfo[i];
-		std::cout << '\t' << i + 1 << ": ";
+
 		// god damn it microsoft why can't you be normal
 		std::wcout << info.strInterfaceDescription << std::endl;
+
+		const GUID& guid = info.InterfaceGuid;
+
+		// this requests a refresh on the list of detected wifi networks
+		hr = WlanScan(handle, &guid, nullptr, nullptr, nullptr);
+
+		if (FAILED(hr))
+		{
+			std::cout << "Warning: WlanScan failed with error code " << hr << std::endl;
+		}
+		else
+		{
+			constexpr auto now = []() -> auto
+			{
+				return high_resolution_clock::now();
+			};
+
+			std::cout << "\tWaiting for WlanScan to complete..." << std::endl;
+
+			// microsoft recommends waiting for (and requires that drivers only take)
+			// four seconds to complete the scan, so we're gonna bail after that if
+			// we don't hear back from the callback.
+			const auto start = now();
+
+			while (!scan_complete)
+			{
+				if (now() - start >= 4s)
+				{
+					std::cout << "\tWarning: WlanScan took too long to complete." << std::endl;
+					break;
+				}
+
+				std::this_thread::sleep_for(1ms);
+			}
+
+			// this is why printf is better
+			std::cout << "\tElapsed time: " << duration_cast<seconds>(now() - start).count() << "s" << std::endl;
+		}
+
+		// note that WlanGetAvailableNetworkList2 (*2*) does not exist on Windows 7
+		// (not that we needed the info provided by it anyway, but LET IT BE KNOWN)
+		PWLAN_AVAILABLE_NETWORK_LIST available_networks = nullptr;
+		hr = WlanGetAvailableNetworkList(handle, &guid, 0, nullptr, &available_networks);
+
+		if (FAILED(hr))
+		{
+			std::cout << "\tWlanGetAvailableNetworkList failed with error code: " << hr << std::endl;
+		}
+		else
+		{
+			for (size_t n = 0; n < available_networks->dwNumberOfItems; ++n)
+			{
+				const auto& network = available_networks->Network[n];
+
+				std::cout << '\t' << n + 1 << ": ";
+
+				if (network.strProfileName[0] != L'\0')
+				{
+					std::cout << "[connected] ";
+				}
+
+				if (network.dot11Ssid.uSSIDLength < 2 && network.dot11Ssid.ucSSID[0] == '\0')
+				{
+					std::cout << "[hidden]";
+				}
+				else
+				{
+					std::cout << network.dot11Ssid.ucSSID;
+				}
+
+				std::cout << " [length: " << network.dot11Ssid.uSSIDLength << "]" << std::endl;
+			}
+		}
+
+		WlanFreeMemory(available_networks);
+		std::cout << std::endl;
 	}
-
-	std::cout << std::endl;
-
-	// blindly select the first one because reasons
-	GUID guid = interface_list->InterfaceInfo[0].InterfaceGuid;
 
 	WlanFreeMemory(interface_list);
 	interface_list = nullptr;
 
-	// this requests a refresh on the list of detected wifi networks
-	hr = WlanScan(handle, &guid, nullptr, nullptr, nullptr);
-
-	if (FAILED(hr))
-	{
-		std::cout << "Warning: WlanScan failed with error code " << hr << std::endl;
-	}
-	else
-	{
-		constexpr auto now = []() -> auto
-		{
-			return high_resolution_clock::now();
-		};
-
-		std::cout << "Waiting for WlanScan to complete..." << std::endl;
-
-		// microsoft recommends waiting for (and requires that drivers only take)
-		// four seconds to complete the scan, so we're gonna bail after that if
-		// we don't hear back from that callback.
-		const auto start = now();
-
-		while (!scan_complete)
-		{
-			if (now() - start >= 4s)
-			{
-				std::cout << "Warning: WlanScan took too long to complete." << std::endl;
-				break;
-			}
-
-			std::this_thread::sleep_for(1ms);
-		}
-
-		// this is why printf is better
-		std::cout << "Elapsed time: " << duration_cast<seconds>(now() - start).count() << "s" << std::endl;
-	}
-
-	// note that WlanGetAvailableNetworkList2 (*2*) does not exist on Windows 7
-	// (not that we needed the info provided by it anyway, but LET IT BE KNOWN)
-	PWLAN_AVAILABLE_NETWORK_LIST available_networks = nullptr;
-	hr = WlanGetAvailableNetworkList(handle, &guid, 0, nullptr, &available_networks);
-
-	if (FAILED(hr))
-	{
-		std::cout << "WlanGetAvailableNetworkList failed with error code: " << hr << std::endl;
-	}
-	else
-	{
-		for (size_t i = 0; i < available_networks->dwNumberOfItems; ++i)
-		{
-			const auto& network = available_networks->Network[i];
-
-			std::cout << i + 1 << ": ";
-
-			if (network.strProfileName[0] != L'\0')
-			{
-				std::cout << "[connected] ";
-			}
-
-			if (network.dot11Ssid.uSSIDLength < 2 && network.dot11Ssid.ucSSID[0] == '\0')
-			{
-				std::cout << "[hidden]";
-			}
-			else
-			{
-				std::cout << network.dot11Ssid.ucSSID;
-			}
-
-			std::cout << " [length: " << network.dot11Ssid.uSSIDLength << "]" << std::endl;
-		}
-	}
-
-	WlanFreeMemory(available_networks);
 	WlanCloseHandle(handle, nullptr);
-
-	std::cout << std::endl << "Press enter to exit." << std::endl;
+	std::cout << "Press enter to exit." << std::endl;
 	getchar();
 	return 0;
 }
